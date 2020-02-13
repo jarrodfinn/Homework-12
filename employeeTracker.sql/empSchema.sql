8d3fc467e3b26c8d772e2d6539b318876242e954
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-02-13 15:19:18 -0500

    created role titles(positions)

--- employeeTracker.sql/empSchema.sql
+++ employeeTracker.sql/empSchema.sql
@@ -22,6 +22,11 @@ VALUES ("Finance");
 INSERT INTO department(name);
 VALUES ("Legal");
 
+-- Sales -department - 1;
+-- Engineering -department - 2;
+-- Finance -department - 3;
+-- Legal -department - 4;
+
 
 CREATE TABLE role(
     id INT AUTO_INCREMENT NOT NULL,
@@ -30,8 +35,28 @@ CREATE TABLE role(
     department_id INT NOT NULL,
     PRIMARY KEY(id)
 );
-INSERT INTO role();
-VALUES ();
+INSERT INTO role(title, salary, department_id);
+VALUES ("Sales Lead", 100000, 1);
+
+INSERT INTO role(title, salary, department_id);
+VALUES ("Salesperson", 80000, 1);
+
+INSERT INTO role(title, salary, department_id);
+VALUES ("Lead Engineer", 150000, 2);
+
+INSERT INTO role(title, salary, department_id);
+VALUES ("Software Engineer", 125000, 2);
+
+INSERT INTO role(title, salary, department_id);
+VALUES ("Accountant", 120000, 3);
+
+INSERT INTO role(title, salary, department_id);
+VALUES ("Legal Team Lead", 250000, 4);
+
+INSERT INTO role(title, salary, department_id);
+VALUES ("Lawyer", 190000, 4);
+
+
 
 
 CREATE TABLE employee (
