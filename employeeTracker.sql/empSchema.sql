deaec17ef72cdb83b9e87ef9828a049d26f67c0d
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-02-13 14:08:46 -0500

    created sql database file

---
+++ employeeTracker.sql/empSchema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS empSchema;
+
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department(
+    
+);
+CREATE TABLE role(
+
+);
+CREATE TABLE employee (
+    
+);
