61279561c873c54f2327baf701b3d28917972b04
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-02-13 14:23:17 -0500

    created tables: department, role, employee

--- employeeTracker.sql/empSchema.sql
+++ employeeTracker.sql/empSchema.sql
@@ -4,12 +4,30 @@ CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE department(
-    
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
 );
-CREATE TABLE role(
 
+CREATE TABLE role(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id)
 );
+
 CREATE TABLE employee (
-    
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY(id)
 );
+
+-- Build a command-line application that at a minimum allows the user to:
+-- Add departments, roles, employees
+-- View departments, roles, employees
+-- Update employee roles
