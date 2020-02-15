d93f77f20fc86b3dd069423e5525950fb323db3e
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-02-15 17:35:27 -0500

    completed file.sql and created file.js

--- employeeTracker.sql/empSchema.sql
+++ employeeTracker.sql/empSchema.sql
@@ -4,71 +4,93 @@ CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE department (
-  id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id)
+CREATE TABLE department( 
+    id INT
+    AUTO_INCREMENT NOT NULL,
+    name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO department(name);
-VALUES ("Sales");
+    INSERT INTO department (name);
+    VALUES ("Sales", "Engineering", "Finance", "Legal");
 
-INSERT INTO department(name);
-VALUES ("Engineering");
-
-INSERT INTO department(name);
-VALUES ("Finance");
-
-INSERT INTO department(name);
-VALUES ("Legal");
-
--- Sales -department - 1;
--- Engineering -department - 2;
--- Finance -department - 3;
--- Legal -department - 4;
+    -- Sales -department - 1;
+    -- Engineering -department - 2;
+    -- Finance -department - 3;
+    -- Legal -department - 4;
 
 
-CREATE TABLE role(
-    id INT AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY(id)
+    CREATE TABLE role (
+        id INT AUTO_INCREMENT NOT NULL,
+        title VARCHAR 30) NOT NULL,
+        salary DECIMAL
+        (10, 2) NOT NULL,
+        department_id INT NOT NULL,
+        PRIMARY KEY (id)
 );
-INSERT INTO role(title, salary, department_id);
-VALUES ("Sales Lead", 100000, 1);
+        INSERT INTO role (title, salary, department_id);
+        VALUES ("Sales Lead", 100000, 1);
 
-INSERT INTO role(title, salary, department_id);
-VALUES ("Salesperson", 80000, 1);
+        INSERT INTO role (title, salary, department_id);
+        VALUES ("Salesperson", 80000, 1);
 
-INSERT INTO role(title, salary, department_id);
-VALUES ("Lead Engineer", 150000, 2);
+        INSERT INTO role (title, salary, department_id);
+        VALUES ("Lead Engineer", 150000, 2);
 
-INSERT INTO role(title, salary, department_id);
-VALUES ("Software Engineer", 125000, 2);
+        INSERT INTO role (title, salary, department_id);
+        VALUES ("Software Engineer", 125000, 2);
 
-INSERT INTO role(title, salary, department_id);
-VALUES ("Accountant", 120000, 3);
+        INSERT INTO role (title, salary, department_id);
+        VALUES ("Account Manager", 160000, 3);
 
-INSERT INTO role(title, salary, department_id);
-VALUES ("Legal Team Lead", 250000, 4);
+        INSERT INTO role (title, salary, department_id);
+        VALUES ("Accountant", 120000, 3);
 
-INSERT INTO role(title, salary, department_id);
-VALUES ("Lawyer", 190000, 4);
+        INSERT INTO role (title, salary, department_id);
+        VALUES ("Legal Team Lead", 250000, 4);
+
+        INSERT INTO role (title, salary, department_id);
+        VALUE ("Lawyer", 190000, 4);
 
 
 
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY(id)
+    CREATE TABLE employee(
+        id INT AUTO_INCREMENT NOT NULL,
+        first_name VARCHAR (30) NOT NULL,
+        last_name VARCHAR (30) NOT NULL,
+        role_id INT NOT NULL,
+        manager_id INT NOT NULL,
+        PRIMARY KEY (id)
 );
-INSERT INTO employees();
-VALUES ();
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ("Julia", "Spieler", 1, null);
+
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ("Jarrod", "Finn", 2, null);
+
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ("Ryan", "Ravenscroft", 3, null);
+
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ("Ashley", "Everett", 4, null);
+
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ("Jan", "Finn", 5, null);
+
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES  ("Austin", "Bickford", 6, null);
+
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES  ("Ian", "Rubin", 7, null);
+            
+            INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ("Jane", "Rubin", 8, null);
+            
+            INSERT INTO employee  (first_name, last_name, role_id, manager_id)
+            VALUES ("Amanda", "Hugankiss", 8, null);
+
+
 
 
 -- Build a command-line application that at a minimum allows the user to:
---
+++ index.js
@@ -0,0 +1,94 @@
+// Build a command-line application that at a minimum allows the user to:
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "rootroot",
+  database: ""
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    startUp();
+  });
+
+
+// Add departments, roles, employees
+
+// View departments, roles, employees
+
+// Update employee roles
+
+
+function startUp() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View All Employees":
+            employeeSearch();
+            break;
+  
+        case "View All Employees By Department":
+            deptEmpSearch();
+            break;
+  
+        case "View All Employees By Manager":
+            managerEmpSearch();
+            break;
+  
+        case "Add Employee":
+            addEmp();
+            break;
+          
+        case "Remove Employee":
+            removeEmp();
+            break;
+            
+        case "Update Employee Role":
+            updateEmpRole();
+            break;
+        
+        case "Update Employee Manager":
+            updateEmpManager();
+            break;
+      
+        case "exit":
+          connection.end();
+          break;
+        }
+      });
+  }
+// Bonus points if you're able to:
+    // Update employee managers
+    // View employees by manager
+    // Delete departments, roles, and employees
+    // View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+
+
+
+
+  
