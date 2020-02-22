1823502b0293520f893710735a9a0763b9ec0b91
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-02-22 12:31:45 -0500

    all searches work

--- employeeTracker.sql/empSchema.sql
+++ employeeTracker.sql/empSchema.sql
@@ -1,7 +1,5 @@
 DROP DATABASE IF EXISTS employees;
-
 CREATE DATABASE employees;
-
 USE employees;
 
 CREATE TABLE department( 
@@ -9,15 +7,8 @@ id INT AUTO_INCREMENT NOT NULL,
 name VARCHAR (30) NOT NULL,
 PRIMARY KEY (id)
 );
-
-INSERT INTO department (name);
-VALUES ("Sales", "Engineering", "Finance", "Legal");
-
--- Sales -department - 1;
--- Engineering -department - 2;
--- Finance -department - 3;
--- Legal -department - 4;
-
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
 CREATE TABLE role (
 id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -26,79 +17,49 @@ salary DECIMAL(10, 2) NOT NULL,
 department_id INT NOT NULL,
 CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
-
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1);
-
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUES ("Salesperson", 80000, 1);
-
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUES ("Lead Engineer", 150000, 2);
-
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUES ("Software Engineer", 125000, 2);
-
-INSERT INTO role (title, salary, department_id);
-VALUES ("Account Manager", 160000, 3);
-
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Account Manager", 160000,3);
+INSERT INTO role (title, salary, department_id)
 VALUES ("Accountant", 120000, 3);
-
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUES ("Legal Team Lead", 250000, 4);
-
-INSERT INTO role (title, salary, department_id);
-VALUE ("Lawyer", 190000, 4);
-
-
-
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 4);
 
 CREATE TABLE employee(
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR (30) NOT NULL,
 last_name VARCHAR (30) NOT NULL,
-role_id INT NOT NULL,
-manager_id INT UNSIGNED,
+role_id INT NULL,
+manager_id INT,
 CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
 CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
-
 );
-
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, null);
-
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Julia", "Spieler", 1, null);
-
+VALUES ("Julia", "Spieler", 2, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jarrod", "Finn", 2, null);
-
+VALUES ("Jarrod", "Finn", 3, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ryan", "Ravenscroft", 3, null);
-
+VALUES ("Ryan", "Ravenscroft", 4, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ashley", "Everett", 4, null);
-
+VALUES ("Ashley", "Everett", 5, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jan", "Finn", 5, null);
-
+VALUES ("Jan", "Finn", 6, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Austin", "Bickford", 6, null);
-
+VALUES  ("Austin", "Bickford", 7, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Ian", "Rubin", 7, null);
-
+VALUES  ("Ian", "Rubin", 8, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jane", "Rubin", 8, null);
-
+VALUES ("Jane", "Rubin", 5, 4);
 INSERT INTO employee  (first_name, last_name, role_id, manager_id)
-VALUES ("Amanda", "Hugankiss", 9, null);
-
-
-
-
--- Build a command-line application that at a minimum allows the user to:
--- Add departments, roles, employees
--- View departments, roles, employees
--- Update employee roles
+VALUES ("Amanda", "Hugankiss", 6, null);
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Build a command-line application that at a minimum allows the user to:
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+// const confirm = require('inquirer-confirm');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    // Your port; if not 3306
     port: 3306,
-    // Your username
     user: "root",
-    // Your password
     password: "rootroot",
     database: "employees"
 });
@@ -18,10 +16,15 @@ connection.connect(function (err) {
     startUp();
 });
 
-
 // Add departments, roles, employees
 // View departments, roles, employees
 // Update employee roles
+
+// Bonus points if you're able to:
+// Update employee managers
+// View employees by manager
+// Delete departments, roles, and employees
+// View the total utilized budget of a department -- ie the combined salaries of all employees in that department
 
 
 function startUp() {
@@ -32,89 +35,89 @@ function startUp() {
             message: "What would you like to do?",
             choices: [
                 "View All Employees",
-                "View All Employees By Department",
-                "View All Employees By Manager",
+                "View All Department",
+                "View All Roles",
                 "Add Employee",
-                "Remove Employee",
+                "Add Department",
+                "Add Role",
                 "Update Employee Role",
-                "Update Employee Manager",
                 "exit"
             ]
         })
         .then(function (answer) {
             switch (answer.action) {
                 case "View All Employees":
-                    employeeSearch();
+                    employeeAllSearch();
                     break;
-
-                case "View All Employees By Department":
-                    deptEmpSearch();
+                case "View All Department":
+                    deptSearch();
                     break;
-
-                case "View All Employees By Manager":
-                    managerEmpSearch();
+                case "View All Roles":
+                    roleSearch();
                     break;
-
                 case "Add Employee":
                     addEmp();
                     break;
-
-                case "Remove Employee":
-                    removeEmp();
+                case "Add Department":
+                    addDept();
                     break;
-
+                case "Add Role":
+                    addRole();
+                    break;
                 case "Update Employee Role":
                     updateEmpRole();
                     break;
-
-                case "Update Employee Manager":
-                    updateEmpManager();
-                    break;
-
                 case "exit":
                     connection.end();
                     break;
             }
         });
-}
-// Bonus points if you're able to:
-// Update employee managers
-// View employees by manager
-// Delete departments, roles, and employees
-// View the total utilized budget of a department -- ie the combined salaries of all employees in that department
-function employeeSearch() {
-
-
-
-    var query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT('manager.first_name', ' ', 'manager.last_name') AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN manager employee.manager_id = employee.id ?";
-
-    connection.query(query, { artist: answer.artist }, function (err, res) {
+};
+function employeeAllSearch() {
+    connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function (err, res) {
         if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-        }
-        runSearch();
+        console.table(res);
+        startUp();
     });
-});
+};
+function deptSearch() {
+    connection.query("SELECT * from department", function (err, res) {
+        if (err) throw err;
 
-function deptEmpSearch() {
+      console.table(res);
+      startUp();
+    })  
+};
+
+function roleSearch() {
+    connection.query("SELECT * from role", function (err, res) {
+        if (err) throw err;
+
+      console.table(res);
+      startUp();
+    })  
 
 };
-function managerEmpSearch() {
 
-};
 function addEmp() {
-    // What is the employee's first name?
-    // What is the employee's last name?
-    // What is the employee's role? Choices: Sales Lead, Sales
-
 };
-function removeEmp() {
 
+function addDept(){
 };
+
 function updateEmpRole() {
-
 };
-function updateEmpManager() {
 
-};
+// function menuOrStartup() {
+//     confirm("Would you like to continue?")
+//     .then(function confirmed() {
+//       showmenu();
+//     }, function cancelled() {
+//       end();
+//     });
+//   }
+  function end() {
+    console.log("Thank you for using Employee Tracker!");
+    connection.end();
+    process.exit();
+  }
